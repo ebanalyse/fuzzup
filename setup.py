8aceeb8539ba215a7ca206b354d6750f1d3708ae
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2022-03-17 09:27:13 +0100

    modify showcase with example

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fuzzup", 
-    version="0.1.3",
+    version="0.1.5",
     author="Lars Kjeldgaard",
     author_email="lars.kjeldgaard@eb.dk",
     description="A Fuzzy Matching Approach for Clustering Strings",

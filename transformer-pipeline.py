9a3ee09a0ec9e16ec99225bada701ba7f4061929
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2022-03-25 13:54:32 +0100

    ner pipeline

--- transformer-pipeline.py
+++ transformer-pipeline.py
@@ -1,49 +1,45 @@
-from transformers import pipeline
+import logging
+
+from ner.inference.predicter import NERPredicter
+from rapidfuzz.fuzz import ratio, partial_token_set_ratio, token_set_ratio
 import pandas as pd
 
-from rapidfuzz.fuzz import partial_token_set_ratio
-from fuzzup.fuzz import fuzz_it_up
+from fuzzup.fuzz import fuzzy_cluster, compute_prominence
+from fuzzup.whitelists import (
+    Cities, 
+    Municipalities, 
+    Neighborhoods, 
+    apply_whitelists, 
+    format_output
+)
+from utils import get_news_data, clean_text
 
-TEXT = """
-EU fremlægger som varslet nu en række muligheder for sanktioner mod Rusland.
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-Det fremgår af en udtalelse.
+# instantiate and load model
+predicter = NERPredicter()
+predicter.load_model('./Bizou/checkpoint-25000/')
+c = Cities()
+m = Municipalities()
+n = Neighborhoods()
+whitelists = [c,m,n]
 
-I 'pakken', som det kaldes i udtalelsen, er der specifikt fire forslag.
+# get article
+article = get_news_data([9150838],
+                        cols=["article_id", "title", "subtitle", "body_text"])
+ 
+text = ",".join(article[["title", "subtitle", "body_text"]].values.tolist()[0])
 
-Sanktionerne skal ramme dem, der var involveret i den 'ulovlige' beslutning at anerkende udbryderregionerne.
+text = clean_text(text)
+preds = predicter.predict(text=text, sentence_based=True)
 
-Også banker, der finansierer det russiske militær og andre operationer i området skal rammes af EU's hammer.
+clusters = fuzzy_cluster(preds, scorer=partial_token_set_ratio, cutoff=75)
 
-Derudover foreslår EU at ramme Ruslands adgang til EU's kapital- og finansielle markeder for at begrænse finansieringen af 'eskalerende og aggressive tiltag'.
+clusters = compute_prominence(clusters, weight_position=0.5)
 
-Til sidst vil EU også ramme handel i de to udbryderregioner til og fra EU.
-"""
-
-ner = pipeline(task='ner', 
-                model='saattrupdan/nbailab-base-ner-scandi', 
-                aggregation_strategy='first')
-ner_output = ner(TEXT)
-
-def tester(ner_output: list, to_dataframe: bool=False):
-    ents = pd.DataFrame.from_records(ner_output)
-    ents = ents.groupby("entity_group", dropna=False)
-    out = {}
-    for entity_type in ents.groups:
-        group = ents.get_group(entity_type)
-        clusters, _ = fuzz_it_up(group.word.tolist(), 
-                                 scorer=partial_token_set_ratio, 
-                                 to_dataframe=to_dataframe)
-        out[entity_type] = clusters 
-    #if to_dataframe:
-    #    out = pd.concat(out, ignore_index=True)
-    return out
-
-t = tester(ner_output)
-        
-    
-    
-    
+matches = apply_whitelists(whitelists, clusters, scorer=partial_token_set_ratio, score_cutoff=95)
+matches
 
 
 

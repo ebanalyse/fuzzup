93c8c6a2c434952a8064ec4cc4270d6ba52944ad
websorz <sorenkolbye2@hotmail.com>
2022-04-06 10:26:04 +0200

    added ability to compute accuracy for prominence

--- JK_experiments.py
+++ JK_experiments.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from tqdm import tqdm
 from typing import List, Dict
 import numpy as np
-from fuzzup.fuzz import fuzzy_cluster_bygroup, fuzzy_cluster
+from fuzzup.fuzz import fuzzy_cluster_bygroup, fuzzy_cluster, compute_prominence_bygroup
 from rapidfuzz.fuzz import partial_token_set_ratio, ratio
 
 # simulate data
@@ -29,13 +29,23 @@ ACCURACIES = {}
 HITS = 0
 MISSES = 0
 
+PROM_HIT = 0
+PROM_MISS = 0
+
 train = pd.read_csv('./prominence_dataset.csv')
+
+# Fuzzy Params
+WEIGHT_POS = 0.5
+CUTOFF = 90
+SCORER = ratio
 
 def _predict(preds:List[Dict]) -> Dict:
     res = defaultdict(lambda:{})
-    fuzzy_preds = fuzzy_cluster_bygroup(preds, cutoff = 90, scorer=ratio)
+    fuzzy_preds = fuzzy_cluster_bygroup(preds, cutoff = CUTOFF, scorer=SCORER)
+    fuzzy_preds = compute_prominence_bygroup(fuzzy_preds, weight_position = None)
+    
     for i, fuzzy_pred in enumerate(fuzzy_preds):
-        res[i].update({'word':fuzzy_pred['word'],'pred':fuzzy_pred['cluster_id'],'true_value':fuzzy_pred['true_value']})
+        res[i].update({'word':fuzzy_pred['word'],'pred':fuzzy_pred['cluster_id'],'true_value':fuzzy_pred['true_value'], 'prominence_rank':fuzzy_pred['prominence_rank'], 'prominent': fuzzy_pred['prominent']})
     return res
 
 def process_dataset() -> List[Dict]:
@@ -45,7 +55,7 @@ def process_dataset() -> List[Dict]:
             pbar.update(1)
             preds_list = []
             for index,row in train[train['content_id']==article].iterrows():
-                pred = {'entity_group': row['entity_group'], 'word': row['word'], 'start': row['start'], 'end': row['end'], 'true_value': row['main_entity']}            
+                pred = {'entity_group': row['entity_group'], 'word': row['word'], 'start': row['start'], 'end': row['end'], 'true_value': row['main_entity'], 'prominent': row['prominent']}            
                 preds_list.append(pred)
             preds_dict[article].update(_predict(preds_list))
     return preds_dict  
@@ -56,11 +66,15 @@ def process_preds_dict(preds_dict : Dict):
         word = []
         cluster_pred = []
         cluster_true = []
+        cluster_prom = []
+        cluster_prom_pred = []
         for val in preds_dict[article].values():
             word.append(val['word'])
             cluster_pred.append(val['pred'])
             cluster_true.append(val['true_value'])
-        data = {'word': word,'cluster_pred':cluster_pred,'cluster_true':cluster_true}
+            cluster_prom.append(val['prominent'])
+            cluster_prom_pred.append(val['prominence_rank'])
+        data = {'word': word,'cluster_pred':cluster_pred,'cluster_true':cluster_true, 'prominent':cluster_prom, 'prominence_rank': cluster_prom_pred}
         ACCURACIES[article] = {'performance':compute_hits_misses(data),'mismatches': set(cluster_pred)-set(cluster_true)}
     
 # count most frequent cluster label
@@ -83,9 +97,33 @@ def retrieve_bow(bow_pred: List, bow_true: List):
         print(bow_pred - bow_true)
         pass
     return (hits, misses)
+
+def compute_most_prominent_loc(df: pd.DataFrame):
+    """This Method will compute whether the identified most prominent entity,
+    really is the most prominent.
+
+    Args:
+        data (Dict): Dictionary of data to evaluate
+
+    Returns:
+        int: some measure of accuracy 
+        """
+    global PROM_MISS
+    global PROM_HIT    
+    
+    #Make sure we only look at what hte model says is the most prominent.
+    df = df[df['prominence_rank'] == 1]
+    
+    for index, row in df.iterrows():
+        if int(row['prominence_rank']) == int(row['prominent']):
+            PROM_HIT += 1
+        else:
+            PROM_MISS += 1                
+        
     
 def compute_hits_misses(data: Dict):
     df = pd.DataFrame.from_dict(data)
+    compute_most_prominent_loc(df)
     global HITS
     global MISSES
     
@@ -105,5 +143,6 @@ preds_dict = process_dataset()
 process_preds_dict(preds_dict)
 
 print(f"# Hits: {HITS}, # Misses: {MISSES}, Modified accuracy: {round(HITS/(HITS+MISSES), 2)}")
+print(f"# Prominent hits: {PROM_HIT}, # Prominent Miss: {PROM_MISS}, Modified accuracy: {round(PROM_HIT/(PROM_HIT+PROM_MISS), 2)}")
 
 __import__('pdb').set_trace()

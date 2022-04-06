ca3cc7e2b32383ae409b61c8e53e20453880b522
websorz <sorenkolbye2@hotmail.com>
2022-04-06 14:10:57 +0200

    added bayesian optmization

--- performance.py
+++ performance.py
@@ -2,30 +2,51 @@ from multiprocessing.sharedctypes import Value
 import pandas as pd
 from collections import defaultdict
 from tqdm import tqdm
-from typing import List, Dict
+from typing import List, Dict, Callable
 import numpy as np
 from fuzzup.fuzz import fuzzy_cluster_bygroup, fuzzy_cluster, compute_prominence_bygroup
-from rapidfuzz.fuzz import partial_token_set_ratio, ratio
+from rapidfuzz.fuzz import (
+    ratio,
+    partial_ratio,
+    partial_ratio_alignment,
+    token_sort_ratio,
+    partial_token_sort_ratio,
+    token_set_ratio,
+    partial_token_set_ratio,
+    token_ratio,
+    partial_token_ratio,
+    WRatio,
+    QRatio,
+)
+
+FUZZUP_SCORERS = {
+    0: ratio,
+    1: partial_ratio,
+    2: partial_ratio_alignment,
+    3: QRatio,
+}
+
+import matplotlib.pyplot as plt
+
 from bayes_opt import BayesianOptimization, UtilityFunction
 
-PARAMS = {'weight_pos' : 0.75,
-          'cutoff': 0.90,
+params = {'weight_pos' : 0.75,
+          'cutoff': 0.1,
           'scorer':ratio
           }
 
 train = pd.read_csv('./prominence_dataset.csv')
 
-def _predict(preds:List[Dict]) -> Dict:
-    global PARAMS
+def _predict(preds:List[Dict], cutoff,scorer) -> Dict:
     res = defaultdict(lambda:{})
-    fuzzy_preds = fuzzy_cluster_bygroup(preds, cutoff = PARAMS['cutoff'], scorer=PARAMS['scorer'])
-    fuzzy_preds = compute_prominence_bygroup(fuzzy_preds, weight_position = PARAMS['weight_pos'])
+    fuzzy_preds = fuzzy_cluster_bygroup(preds, cutoff = cutoff, scorer=FUZZUP_SCORERS[scorer])
+    fuzzy_preds = compute_prominence_bygroup(fuzzy_preds, weight_position = params['weight_pos'])
     
     for i, fuzzy_pred in enumerate(fuzzy_preds):
         res[i].update({'word':fuzzy_pred['word'],'pred':fuzzy_pred['cluster_id'],'true_value':fuzzy_pred['true_value'], 'prominence_rank':fuzzy_pred['prominence_rank'], 'prominent': fuzzy_pred['prominent']})
     return res
 
-def process_dataset(train: pd.DataFrame) -> List[Dict]:
+def process_dataset(train: pd.DataFrame, cutoff, scorer) -> List[Dict]:
     preds_dict = defaultdict(lambda: {})
     with tqdm(total=train['content_id'].nunique()) as pbar:
         for article in np.unique(train['content_id']):
@@ -34,7 +55,7 @@ def process_dataset(train: pd.DataFrame) -> List[Dict]:
             for index,row in train[train['content_id']==article].iterrows():
                 row['true_value'] = row['main_entity']
                 preds_list.append(dict(row))
-            preds_dict[article].update(_predict(preds_list))
+            preds_dict[article].update(_predict(preds_list, cutoff,scorer))
     return preds_dict  
 
 def evaluate_fuzzy_matching(preds_dict: Dict, mode: str = 'jaccard') -> float:
@@ -83,6 +104,60 @@ def evaluate_prominence(preds_dict: Dict, mode: str = 'jaccard') -> float:
     
     else:
         raise ValueError('Pleaes pass mode string as either "dice" or "jaccard"')
-preds_dict = process_dataset(train)
-accuracy, diagnosis_dict = evaluate_fuzzy_matching(preds_dict)
+# preds_dict = process_dataset(train, cutoff = 0, scorer=ratio)
+# accuracy, diagnosis_dict = evaluate_fuzzy_matching(preds_dict)
 
+# print(f'ACCURACY: {accuracy} \n')
+# print(f'Number of articles with incorrect predictions: {len(diagnosis_dict)}')
+
+def black_box_function(cutoff: int, scorer):
+    preds_dict = process_dataset(train, cutoff=cutoff, scorer=scorer)
+    accuracy, diagnosis_dict = evaluate_fuzzy_matching(preds_dict)
+    return accuracy
+
+
+pbounds = {
+          "cutoff": [0,100],
+          "scorer": [0,len(FUZZUP_SCORERS)-1]
+          }
+
+
+optimizer = BayesianOptimization(f = black_box_function,
+                                 pbounds = pbounds, verbose = 2,
+                                 random_state = 4)
+
+utility = UtilityFunction(kind="ucb", kappa = 1.96, xi = 0.01)
+
+#TRAINING LOOP
+for i in range(25):
+    next_point = optimizer.suggest(utility)
+    
+    next_point['scorer'] = int(next_point['scorer'])
+    
+    target = black_box_function(cutoff = next_point['cutoff'], scorer = next_point['scorer'])
+    
+    try:
+        optimizer.register(params=next_point, target=target)
+    except:
+        pass    
+    print(f'TARGET: {target} \n')
+    print(f'cutoff: {next_point["cutoff"]} \n  scorer: {FUZZUP_SCORERS[next_point["scorer"]]} ' )
+
+print("Best result: {}; f(x) = {:.3f}.".format(optimizer.max["params"], 
+                                               optimizer.max["target"]))
+
+plt.figure(figsize = (15, 5))
+plt.plot(range(1, 1 + len(optimizer.space.target)), optimizer.space.target, "-o")
+plt.grid(True)
+plt.xlabel("Iteration", fontsize = 14)
+plt.ylabel("Black box function f(x)", fontsize = 14)
+plt.xticks(fontsize = 14)
+plt.yticks(fontsize = 14)
+plt.show()
+
+
+# optimizer.maximize()
+# print("Best result: {}; f(x) = {}.".format(optimizer.max["params"], optimizer.max["target"]))
+
+
+# __import__('pdb').set_trace()

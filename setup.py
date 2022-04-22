887d40b1f7e554798d257242c7439b002716afdb
Soren Jensen <sorenkolbye2@hotmail.com>
2022-04-22 11:41:53 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fuzzup", 
-    version="0.3.9",
+    version="0.4.0",
     author="Lars Kjeldgaard",
     author_email="lars.kjeldgaard@eb.dk",
     description="A Fuzzy Matching Approach for Clustering Strings",

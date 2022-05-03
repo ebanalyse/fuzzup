a9aefe4c4f16355e42dd9fe99c5fad59823a855e
websorz <sorenkolbye2@hotmail.com>
2022-05-03 09:05:29 +0200

    some changes

--- optimization/performance.py
+++ optimization/performance.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from typing import List, Dict, Callable
 import numpy as np
 from fuzzup.fuzz import fuzzy_cluster_bygroup, fuzzy_cluster, compute_prominence_bygroup, compute_prominence_placement
+from fuzzup.whitelists import EBLocalNames, Municipalities, apply_whitelists
 from rapidfuzz.fuzz import (
     ratio,
     partial_ratio,
@@ -22,22 +23,22 @@ import matplotlib.pyplot as plt
 
 from bayes_opt import BayesianOptimization, UtilityFunction
 
+import json
+
+import ast
 
 FUZZUP_SCORERS = {
     0: ratio,
     1: partial_ratio,
     2: partial_token_set_ratio,
+    3: WRatio
 }
 
+WHITELISTS = [EBLocalNames(), Municipalities()]
 
-train = pd.read_excel("./annotated_dump.xlsx")
+train = pd.read_excel("./processed_annotated_dump.xlsx")
 
-__import__('pdb').set_trace()
-
-# train = train[train['entity_group'] == 'LOC']
-
-train = train[~train.Kommentar.isna()]
-
+train = train[train['entity_group'] == 'LOC']
 
 def _assert_fuzz_json(json_data):
     if not all (key in json_data for key in ("title","body")):
@@ -65,37 +66,103 @@ def _text_segment_adder(json_data, preds_out):
             pred['text_segment'] = 'body'
     return preds_out
 
-
-def _predict(preds: List[Dict], cutoff, scorer, weight_pos, json_data, **kwargs) -> Dict:
+def flatten_whitelist(wl_clusters) -> List:
+    match_city = []
+    match_municipality = []
+    
+    if len(wl_clusters['eblocal_name'])> 0 or len(wl_clusters['municipality'])> 0:
+        for whitelist in wl_clusters:
+            for matches in wl_clusters[whitelist]:
+                match = matches['matches']
+                for mapping in matches ['mappings']:
+                    if whitelist == 'eblocal_name':
+                        if (match, mapping['eblocal_id']) not in match_city:
+                            match_city.append((match, mapping['eblocal_id']))
+                    if whitelist == 'municipality':
+                        if match not in match_municipality:
+                          match_municipality.append(match)
+    return match_city
+ 
+def _predict(preds: List[Dict],
+             #cutoff_fuzz,
+             cutoff_wl,
+             #scorer_fuzz,
+             scorer_wl,
+             wgt_body,
+             wgt_lead,
+             wgt_title,
+             weight_pos,
+             json_data) -> Dict:
     res = defaultdict(lambda: {})
     
     json_data = _assert_fuzz_json(json_data)
     fuzzy_preds = fuzzy_cluster_bygroup(
-        preds, cutoff=cutoff, scorer=FUZZUP_SCORERS[scorer]
+        preds, cutoff=98, scorer=token_ratio
     )
     
     fuzzy_preds = _text_segment_adder(json_data, fuzzy_preds)
 
     try:
-        fuzzy_preds = compute_prominence_placement(fuzzy_preds, weight_position=weight_pos, **kwargs)
+        fuzzy_preds = compute_prominence_placement(fuzzy_preds, placement_col="text_segment", weight_position=weight_pos, wgt_lead=wgt_lead, wgt_body=wgt_body,wgt_title=wgt_title, return_first_rank = False, bygroup=True)
     except:
         __import__('pdb').set_trace()
+        
+    whitelist_matches = apply_whitelists(WHITELISTS, fuzzy_preds, scorer =FUZZUP_SCORERS[scorer_wl], score_cutoff = cutoff_wl, aggregate_cluster=True)
+    
+    match_city = flatten_whitelist(whitelist_matches)   
+     
+    """
+    TODO:  if fuzzy_pred returns an empty dictionary, then the evaluation goes wrong, as true_value will never be appended either?
+    """
     
     for i, fuzzy_pred in enumerate(fuzzy_preds):
+        #Filter any predictions with a score of lower than 10
+        if fuzzy_pred['prominence_score'] >= 20:
+            continue
+        else:
+            #If the score is lower than 10, then the prediction is nothing
+            match_city = []
         res[i].update(
             {
                 "word": fuzzy_pred["word"],
-                "pred": fuzzy_pred["cluster_id"],
+                "pred": match_city,
                 "true_value": fuzzy_pred["true_value"],
                 "prominence_rank": fuzzy_pred["prominence_rank"],
                 "prominence_score": fuzzy_pred["prominence_score"],
-                "prominent": fuzzy_pred["prominent"],
             }
         )
+    i = 0
+    if len(res) == 0:
+        res[i].update(
+              {
+                "word": fuzzy_pred["word"],
+                "pred": match_city,
+                "true_value": fuzzy_pred["true_value"],
+                "prominence_rank": fuzzy_pred["prominence_rank"],
+                "prominence_score": fuzzy_pred["prominence_score"],
+            }
+        )
+        i += 1
+        
     return res
 
 
-def process_dataset(train: pd.DataFrame, cutoff, scorer, weight_pos=1, eval = 'whitelist',**kwargs) -> List[Dict]:
+def process_dataset(train: pd.DataFrame, #cutoff_fuzz,
+             cutoff_wl,
+         #    scorer_fuzz,
+             scorer_wl,
+             wgt_body,
+             wgt_lead,
+             wgt_title,
+             weight_pos,) -> List[Dict]:
+    
+    if 'subtitle' in train:
+        train = train.rename({'subtitle': 'lead'}, axis = 'columns')
+    if 'body_text' in train:
+        train = train.rename({'body_text': 'body'}, axis = 'columns')
+    if 'article_id' in train:
+        train = train.rename({'article_id':'content_id'}, axis = 'columns')
+    
     preds_dict = defaultdict(lambda: {})
     with tqdm(total=train["content_id"].nunique()) as pbar:
         for article in np.unique(train["content_id"]):
@@ -103,15 +170,99 @@ def process_dataset(train: pd.DataFrame, cutoff, scorer, weight_pos=1, eval = 'w
             preds_list = []
             json_data = {}
             for index, row in train[train["content_id"] == article].iterrows():
-                row["true_value"] = row["main_entity"]
+                row["true_value"] = ast.literal_eval(row['NER-matches_city'])
                 preds_list.append(dict(row))
             json_data['title'] = row['title']
             json_data['body'] = row['body']
             if 'lead' in row is not None:
                 json_data['lead'] = row['lead']
-            preds_dict[article].update(_predict(preds_list, cutoff, scorer, weight_pos, json_data, **kwargs))
+            preds_dict[article].update(_predict(preds_list,
+          #   cutoff_fuzz,
+             cutoff_wl,
+          #   scorer_fuzz,
+             scorer_wl,
+             wgt_body,
+             wgt_lead,
+             wgt_title,
+             weight_pos,
+             json_data))
     return preds_dict
 
+def clean_preds(preds: Dict) -> Dict:
+    clean_preds = []
+    clean_true = []
+    
+    if len(preds['pred']) > 0:
+        for pred in preds['pred']:
+            for entry in pred[0]:
+             clean_preds.append(entry)
+    if len(preds['true_value']) > 0:
+        for pred in preds['true_value']:
+            for entry in pred[0]:
+                clean_true.append(entry)
+        
+    preds['pred'] = clean_preds
+    preds['true_value'] = clean_true
+    return preds
+
+def evaluate_whitelist_matching(preds_dict:Dict,mode:str = 'jaccard') -> float:
+    TN = 0
+    TP = 0
+    FN = 0
+    FP = 0
+    
+    if mode == 'f1':
+        pass
+    
+    
+    if mode == 'jaccard':
+        accuracies = {}
+        diagnosis_dict = {}
+        for article in preds_dict.keys():
+            preds_set = set()
+            true_set = set()
+            for preds in preds_dict[article].values():
+                preds = clean_preds(preds)
+                for pred in preds['pred']:
+                    preds_set.add(pred)
+                for true_val in  preds['true_value']:
+                    true_set.add(true_val)
+
+            if len(preds_set) != 0 and len(true_set) != 0:
+                accuracy = round(
+                    len(preds_set.intersection(true_set))
+                    / len(preds_set.union(true_set)),
+                    2,
+                )
+                if len(preds_set - true_set) != 0:
+                    diagnosis_dict[article] = {
+                        "preds_set": preds_set,
+                        "true_set": true_set,
+                        "misses": preds_set - true_set,
+                    }
+            elif len(preds_set) ==0 and len(true_set) == 0:
+                print('both empty')
+                pass
+            elif len(preds_set) ==0 and not len(true_set):
+                print("pred but no true exist")
+                accuracy = 0
+            elif not len(preds_set) and len(true_set):
+                print('pred empty but true exist')
+                accuracy = 0
+            
+            try:
+                accuracies[article] = accuracy
+            except:
+                continue
+            print('predictions: ' , preds_set, 'true_value: ',  true_set)
+        if len(accuracies) != 0:
+            return (
+                sum(accuracies.values()) / len(accuracies),
+                diagnosis_dict,
+            )  # Return the mean accurac     
+        else:
+            return 0, None       
+                
 def evaluate_fuzzy_matching(preds_dict: Dict, mode: str = "jaccard") -> float:
     """Just loops through the preds dict and aggregates their evaluations"""
 
@@ -225,6 +376,56 @@ def plot_results(optimizer):
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.show()
+    
+def train_whitelist():
+    def black_box_function(**kwargs):
+        preds_dict = process_dataset(train, **kwargs)
+        accuracy, diagnosis_dict = evaluate_whitelist_matching(preds_dict)
+        return accuracy
+
+    pbounds = {#"cutoff_fuzz": [0, 100],
+               "cutoff_wl": [0, 100],
+               #"scorer_fuzz": [0, len(FUZZUP_SCORERS) - 1],
+               "scorer_wl": [0, len(FUZZUP_SCORERS) - 1],
+               "weight_pos": [0,1],
+               "wgt_body": [0,20],
+               "wgt_lead": [0, 20],
+               "wgt_title": [0, 20],
+               }
+
+    optimizer = BayesianOptimization(
+        f=black_box_function, pbounds=pbounds, verbose=2, random_state=4
+    )
+
+    utility = UtilityFunction(kind="ucb", kappa=1.96, xi=0.01)
+
+    # TRAINING LOOP
+    for i in range(25):
+        next_point = optimizer.suggest(utility)
+
+        target = black_box_function(
+           #cutoff_fuzz=next_point["cutoff_fuzz"],
+            #scorer_fuzz=int(next_point["scorer_fuzz"]),
+            cutoff_wl=next_point["cutoff_wl"],
+            scorer_wl=int(next_point["scorer_wl"]),
+            weight_pos=next_point["weight_pos"],
+            wgt_body=next_point["wgt_body"],
+            wgt_lead=next_point["wgt_lead"],
+            wgt_title=next_point["wgt_title"],
+        )
+
+        try:
+            optimizer.register(params=next_point, target=target)
+        except:
+            pass
+        print(f"TARGET: {target} \n")
+        print(next_point)
+
+    plot_results(optimizer)
+    
+    
+    pass
+
 
 
 def train_fuzzy():
@@ -310,7 +511,7 @@ def train_prominent():
     plot_results(optimizer)
 
 
-train_prominent()
+train_whitelist()
 
 # preds_dict = process_dataset(train, cutoff = 0, scorer=ratio)
 # accuracy, diagnosis_dict = evaluate_fuzzy_matching(preds_dict)
